e4902e524e4509777bd554c74ecb3d7b16e2976d
joana <muziranengejoana@gmail.com>
2023-11-28 04:24:35 -0800

    the quiz correction

---
+++ index.js
@@ -0,0 +1,16 @@
+let name1 = "Maxwell wright";
+const phone1 = '(0191) 719 6495';
+let mail1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+let name2 = "Raja Villarreal";
+const phone2 = '0866 398 2895';
+let mail2 = "Posuere.vulputate@sed.com";
+
+
+
+let name3 = "Helen Richards";
+const phone3 = '08001111';
+let mail3 = "libere@convallis.edu";
+
+console.log("Maxwell wright",'/(0191) 719 6495',"/Curabitur.egestas.nunc@nonummyac.co.uk.");
+console.log("Helen Richards",'/08001111',"/libere@convallis.edu");
