fb5e4cbad187854ec60972eabb1cb75ff8e68e9a
joana <muziranengejoana@gmail.com>
2023-11-28 07:04:18 -0800

    my quiz correction updates

---
+++ quiz2/index.js
@@ -0,0 +1,16 @@
+let rosePrice =8;
+let roseQuantity =70;
+let roseTotalPrice =rosePrice*roseQuantity;
+
+let lilyPrice =10;
+let lilyQuantity =50;
+let lilyTotalPrice =lilyPrice*lilyQuantity;
+
+let tulipPrice =2;
+let tulipQuantity =120;
+let tulipTotalPrice =tulipPrice*tulipQuantity;
+
+console.log("rose-unit price:8","quantity:70","value:560");
+console.log("lily-unit price:10","quantity:50","value:500");
+console.log("tulip-unit price:2","quantity:120","value:240");
+console.log("Total:","rosePrice*roseQuantity+lilyPrice*lilyQuantity+tulipPrice*tulipQuantity");
